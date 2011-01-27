46706096e7518b76d2a87ebba285da740bfec7f5
Matthew Soldo <matt@soldo.org>
2011-01-27 00:18:36 -0800

    initial commit (fresh rails project)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inventory::Application.config.secret_token = '95fcf6cd459173b40aded2f1dd52d31242c4c324fbbad055af8f18454eaddff79a942b80d66ceef31c32dfe90520d15a4e5e3452b37271ba8c6f6ed619f01a90'
